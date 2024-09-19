a91f7ac8ca6926740d08f63d7e47c4144bd8449b
EC2 Default User <ec2-user@ip-172-31-27-104.ec2.internal>
2024-09-19 17:59:29 +0000

    Update package.json with test script and jest

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-const http = require('http');
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.end('Hello, DevOps Lifecycle');
-});
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+// app.js
+console.log('App is running');
+
